a660ec755d29dc1c3790d5f3de2aea538e70c939
Vitaliy Siryk <vitaliysiryk@Vitaliys-MacBook-Air.local>
2023-08-29 18:13:32 +0300

    Updated

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,20 @@
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+
+vim.api.nvim_create_user_command('ToggleDiagnostic', function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "Toggle diagnostic" })
+vim.cmd('ToggleDiagnostic')
---
+++ lua/config/options.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.o.hlsearch = false
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.mouse = ''
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
